3f9761136f2d6f89a37584b1de5e1a2c83c3e831
Adam Argyle <atom@argyleink.com>
2013-03-07 20:19:18 -0800

    syntax correction

--- js/impress.js
+++ js/impress.js
@@ -174,8 +174,7 @@
                            
                           // and `classList` and `dataset` APIs
                            ( body.classList ) &&
-                           ( body.dataset ) &&
-                           
+                           ( body.dataset )                            
                           // but some mobile devices need to be blacklisted,
                           // because their CSS 3D support or hardware is not
                           // good enough to run impress.js properly, sorry...

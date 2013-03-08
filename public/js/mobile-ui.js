91e4b147f633a0d5ffbdbe8f1f0076707a62c1f0
Adam Argyle <atom@argyleink.com>
2013-03-08 09:14:56 -0800

    updates

--- public/js/mobile-ui.js
+++ public/js/mobile-ui.js
@@ -1,15 +1,20 @@
 (function(){
 
     var syncWithPresenter 	= true,
-        el 					= document.querySelector('#impress');
+        el 					= document.querySelector('#impress'),
+        supportsVibrate 	= "vibrate" in navigator;
 
-    Hammer(el).on("hold pinch", function() {
+    Hammer(el).on("pinch", function() {
         impress().goto('birdseye');
     });
 
-    Hammer(el).on("doubletap", function() {
+    Hammer(el).on("hold", function() {
+        if (supportsVibrate)
+            navigator.vibrate(200);
+
         syncWithPresenter = syncWithPresenter ? false : true;
         syncWithPresenter ? api.followPresenter() : api.ignorePresenter();
+        
     });
     
 })()
